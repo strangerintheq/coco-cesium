5e2b12bce83e977b7da87a583fe7e8466970103f
Balashev Konstantin <balashev.konstantin@nicetu.spb.ru>
2017-06-07 17:48:41 +0300

    selection indiactor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,7 @@ window.Events = require('./Events');
 window.QWebChannel = require('./QWebChannel');
 window.engine = require('./Engine');
 
+require('./SelectionIndicator');
 require('./UserLayerManager');
 require('./Camera');
 require('./Picker');
